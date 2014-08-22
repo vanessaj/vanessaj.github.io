903d9c2a7e7e7a519dd8c0e4dc6f5bc0511de6bb
Vanessa MacIsaac <vsmacisaac@gmail.com>
2014-08-22 14:39:30 -0400

    drawing on canvas

---
+++ logo.js
@@ -0,0 +1,8 @@
+// javascript for drawing on canvas
+
+var canvas = document.getElementById("logo");
+var context = canvas.getContext("2d");
+
+context.fillText = ("Text", 10, 10);
+
+alert("JS is running!");
